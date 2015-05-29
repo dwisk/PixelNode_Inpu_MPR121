51fbc684348aaa91a49d6381ba4cfbc417cd84cc
Amely Kling <mail@dwi.sk>
2015-05-29 13:59:10 +0200

    Replaced Python-Bridge with a direct implementation via mpr121

--- README.md
+++ README.md
@@ -1,11 +1,2 @@
 # PixelNode_Input_MPR121
-Inputs from Adafruits MPR121 touch sensor via a python subprocess. (Needs Adafruit_Python_MPR121)
-
-## Python Library
-You'll need to install Adafruits python library for it to work. 
-
-You may want to use the forked version, as it handles some minor bugs and allows to use two sensors on two i2c-buses:
-- https://github.com/dwisk/Adafruit_Python_MPR121/tree/dwisk
-
-Otherwise use the original from:
-- https://github.com/adafruit/Adafruit_Python_MPR121
+Inputs from Adafruits MPR121 touch sensor.
--- index.js
+++ index.js
@@ -15,8 +15,7 @@
 
 var util = require("util");
 var _ = require('underscore');
-var spawn = require('child_process').spawn;
-
+var MPR121 = require('mpr121');
 
 /* Class Constructor
  * ==================================================================================================================== */
@@ -46,9 +45,11 @@ PixelNode_Input_MPR121.prototype.default_options = {
 	"crash_cautious_lifetime": 20,
 	"crash_cautious_waittime": 2,
 	"i2c_bus": 2,
+	"i2c_address": 0x5A,
 	"offset": 0,
 	"verbose": false
 };
+PixelNode_Input_MPR121.prototype.touchsensor = null;
 
 var firstInit = 0;
 var lastInit = 0;
@@ -118,56 +119,31 @@ PixelNode_Input_MPR121.prototype.init = function() {
 // start python listener
 PixelNode_Input_MPR121.prototype.start = function(callback) {
 	var self = this;
+ 	
+ 	// setup sensor device 
+ 	self.touchsensor = new MPR121(self.options.i2c_address, self.options.i2c_bus);
 
- 	// spawn pythong process
-	var ls = spawn('python', ['-u', __dirname + '/touch.py', self.options.i2c_bus]);
-	this.callback = callback;
+ 	// initialize sensor, on success start script
+ 	if (self.touchsensor.begin()) {
 
-	// data listener
-	ls.stdout.on('data', function (data) {
-		// get rid of empty lines
-		var str = data.toString().replace("\n","");
+ 		// Interval for reading the sonsor
+ 		setInterval(function() {
+ 			// get touch values
+ 			var t = self.touchsensor.touched();
 
-		// check if result is valid
-		if (str.length > 2) {
-			// parse data
-			var parts = str.replace("[","").replace("]","").split(",");
+ 			// prepare some result array
+ 			var ret = [];
 
-			// get pin data
-			var pins = [];
-			parts.forEach(function(pin) {
-				pins.push( pin == "true");
-			})
+ 			// loop through pins
+ 			for (var i = 0; i < 12; i++) {
+ 				// push status into array
+ 				ret.push (self.touchsensor.is_touched(i));
+ 			}
+ 			
+ 			// return status array
+ 			callback(ret);
 
-			// send pin data to callback
-			callback(pins);
-		}
-	});
-
-	// close listener
-	ls.on('close', function (code) {
-		if (code == 0 ) {
-			console.log('PixelNode_Input_MPR121: python process exited'.grey);
-		} else {
-			// crash stats
-			crash = new Date();
-			crashCount++;
-			lifetime = Math.round((crash - lastInit)/1000);
-			avg_lifetime = Math.round((crash - firstInit)/1000/crashCount);
-
-			// wait time
-			waittime = self.options.crash_waittime;
-			if (lifetime < self.options.crash_cautious_lifetime) waittime = self.options.crash_cautious_waittime;
-
-			// output
-			console.log(('MPR121 crashed after '+ lifetime+ 's (avg ' + avg_lifetime + 's). Restarting in ' + waittime + 's.').yellow);
-
-			// new init after waittime
-			setTimeout(function() {
-				self.init();
-			}, waittime*1000);
-		}
-	});
+ 		},100);
+ 	};
 
 }
-
